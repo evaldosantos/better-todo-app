1fbfb66654b086f39393ea4408decff51fe4818b
Evaldo Santos <jesantosjr@protonmail.com>
2021-01-03 21:27:54 -0300

    initial layout

--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,69 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Todo list</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;700;900&display=swap" rel="stylesheet"></link>
       </Head>
+      <h1 class="title">Stuff I need to do</h1>
+      
+      <div className="all-tasks">
+        <h2 className="task-list-title">My lists</h2>
+        <ul className="task-list">
+          <li className="active-list">Youtube</li>
+          <li>Work</li>
+          <li>Grocery</li>
+        </ul>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <form action="">
+          <input type="text" class="new-list" name="" id="" placeholder="new list name" aria-label="new list name" />
+          <button className="btn btn-list" aria-label="create new list">+</button>
+        </form>
+      </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="todo-list">
+        <div className="todo-header">
+          <h2 className="list-title">Youtube</h2>
+          <p className="task-count">3 tasks remianing</p>
         </div>
-      </main>
+        <div className="todo-body">
+          <div className="tasks">
+            <div className="task">
+              <input type="checkbox" name="" id="task-1"/>
+              <label htmlFor="task-1">
+                <span className="custom-checkbox"></span>
+                record todo list video</label>
+            </div>
+            <div className="task">
+              <input type="checkbox" name="" id="task-1"/>
+              <label htmlFor="task-1">
+                <span className="custom-checkbox"></span>
+                edit todo list video</label>
+            </div>
+            <div className="task">
+              <input type="checkbox" name="" id="task-1"/>
+              <label htmlFor="task-1">
+                <span className="custom-checkbox"></span>
+                publish todo list video</label>
+            </div>
+          </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          <div className="new-task-creator">
+            <form action="">
+              <input type="text" name="" id="" class="new-task" placeholder="new task name" aria-label="new task name" />
+              <button className="btn btn-task" aria-label="create new task">+</button>
+            </form>
+          </div>
+        </div>
+
+
+        <div className="delete-stuff">
+          <button className="btn delete">Clear completed tasks</button>
+          <button className="btn delete">Delete list</button>
+        </div>
+      </div>
+
     </div>
   )
 }
